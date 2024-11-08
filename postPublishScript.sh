8d81f7985920fadaeb6f37e08b803be3e7bc8787
Max <max.xie@zenlayer.com>
2024-11-08 16:36:35 +0800

    添加npm发布前后的脚本文件

---
+++ postPublishScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 自定义脚本内容
+echo "发布完成，执行自定义操作..."
+
+git push origin  main
+
+echo "推送成功。"
+
---
+++ prepublishOnly.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 执行构建、清理工作或者其他操作
+echo "正在执行发布前脚本..."
+
+# 执行打包（比如使用 Babel）
+npm run build
+
+# 如果一切成功，继续执行 npm publish
+echo "准备好发布了!"

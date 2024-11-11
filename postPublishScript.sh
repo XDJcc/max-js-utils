803fb450c0652c0c97f24a265e35260c58ab12ec
Max <max.xie@zenlayer.com>
2024-11-11 16:00:17 +0800

    发布前脚本更新

--- postPublishScript.sh
+++ postPublishScript.sh
@@ -5,5 +5,5 @@ echo "发布完成，执行自定义操作..."
 
 #git push origin  main
 
-echo "推送成功。"
+echo "END！"
 
--- prepublishOnly.sh
+++ prepublishOnly.sh
@@ -4,9 +4,22 @@
 echo "正在执行发布前脚本..."
 
 # 执行打包（比如使用 Babel）
-#npm run build_publish
-#npm run build
 
+  # babel 编译（弃用）
+  #npm run build_publish
+
+#vite编译
+npm run build
+
+
+#删除vite图标
+FILE_PATH="./dist/vite.svg"
+if [ -f "$FILE_PATH" ]; then
+    rm "$FILE_PATH"
+    echo "文件已删除: $FILE_PATH"
+else
+    echo "文件不存在: $FILE_PATH"
+fi
 
 # 如果一切成功，继续执行 npm publish
 echo "准备好发布了!"
